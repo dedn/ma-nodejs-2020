ec81b5422f714fb042396a468c815412a6c8e0ec
Yura <dedn61@gmail.com>
2019-11-12 23:34:50 +0200

    removed tasks from master branch

--- js-check/9.js
+++
@@ -1,9 +0,0 @@
-/**
- * Task 9
- */
-
-const getPromise = (delay) => new Promise(resolve => setTimeout(()=>{
-    resolve('resolved')
-}, delay));
-
-getPromise(2000).then(result => console.log(result));
