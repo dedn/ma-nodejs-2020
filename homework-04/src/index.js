5b56a7f1825346d292d2a82e9f516988f7922dcd
Yura <dedn61@gmail.com>
2019-12-07 23:41:44 +0200

    added solution with callback

--- homework-04/src/index.js
+++ homework-04/src/index.js
@@ -1,3 +1,17 @@
-let  throwDice = () =>{
+const getRandomNumber = () => Math.floor(Math.random() * 6) + 1;
+const throwDice = cb => cb(getRandomNumber());
 
-}
+setTimeout(() => {
+  throwDice(firstThrow => {
+    console.log(`first throw ${firstThrow}`);
+    setTimeout(() => {
+      throwDice(secondThrow => {
+        console.log(`second throw${secondThrow}`);
+        setTimeout(() => {
+          const sum = firstThrow + secondThrow;
+          console.log(`results ${sum}`);
+        }, 700);
+      });
+    }, 2000);
+  });
+}, 3000);
